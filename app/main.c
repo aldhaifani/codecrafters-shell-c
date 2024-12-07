197eb131b4b9c329f95052b21ef614651148960a
Tareq <tareqaldhaifani@gmail.com>
2024-12-07 20:17:33 +0400

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 
 int main() {
-  // Uncomment this block to pass the first stage
-  printf("$ ");
-  fflush(stdout);
 
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
+  // REPL
+  while (1) {
+    printf("$ ");
+    fflush(stdout);
 
-  // Remove newline character
-  for (int i = 0; i < 100; i++) {
-    if (input[i] == '\n') {
-      input[i] = '\0';
-      break;
+    // Wait for user input
+    char input[100];
+    fgets(input, 100, stdin);
+
+    // Remove newline character
+    for (int i = 0; i < 100; i++) {
+      if (input[i] == '\n') {
+        input[i] = '\0';
+        break;
+      }
     }
-  }
-  printf("%s: not found\n", input);
+    printf("%s: not found\n", input);
+   }
 
   return 0;
 }
