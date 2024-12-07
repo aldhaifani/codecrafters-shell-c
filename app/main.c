c33bf02c48c2120965b3f20a51d6c5dd26d71c40
Tareq <tareqaldhaifani@gmail.com>
2024-12-07 20:15:40 +0400

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -8,5 +8,15 @@ int main() {
   // Wait for user input
   char input[100];
   fgets(input, 100, stdin);
+
+  // Remove newline character
+  for (int i = 0; i < 100; i++) {
+    if (input[i] == '\n') {
+      input[i] = '\0';
+      break;
+    }
+  }
+  printf("%s: not found\n", input);
+
   return 0;
 }
